282e23365036627617287b2dd27c570251f3ecca
Vig1lante <apakieser@gmail.com>
2020-08-27 16:07:10 +0200

    Further refactor StudentRepository for fetching user role method

--- QuestShop/Models/StudentRepository.cs
+++ QuestShop/Models/StudentRepository.cs
@@ -12,6 +12,7 @@ namespace QuestShop.Models
     public class StudentRepository : IStudentRepository
     {
         private readonly QuestShopDbContext _questShopDbContext;
+        private readonly UserManager<AppUser> userManager;
         private readonly string _currentUserName;
 
         public AppUser LoggedInUser
@@ -47,9 +48,10 @@ namespace QuestShop.Models
         }
 
 
-        public StudentRepository(QuestShopDbContext questShopDbContext, IHttpContextAccessor httpContextAccessor)
+        public StudentRepository(QuestShopDbContext questShopDbContext, IHttpContextAccessor httpContextAccessor, UserManager<AppUser> userManager)
         {
             _questShopDbContext = questShopDbContext;
+            this.userManager = userManager;
             _currentUserName = httpContextAccessor.HttpContext.User.Identity.Name; //Set current user guid to field
         }
 
@@ -58,19 +60,7 @@ namespace QuestShop.Models
             return _questShopDbContext.Users.FirstOrDefault(s => s.Email == _currentUserName);
         }
 
-        private string GetUserRoleId()
-        {
-            var roleTemp = _questShopDbContext.UserRoles.AsEnumerable();
-            var result = _questShopDbContext.UserRoles.Where(s => s.UserId == LoggedInUser.Id).FirstOrDefault();
-            if (result == null)
-            {
-                return null;
-            }
-            else
-            {
-                return _questShopDbContext.Roles.FirstOrDefault(p => p.Id == result.RoleId).Name;
-            }
-        }
+        private string GetUserRoleId() => userManager.GetRolesAsync(LoggedInUser).Result.First();
 
     }
 }
